a020d562a1a0b17691c97a318f6591840aa441d2
enrique <enrique_graciano@hotmail.com>
2024-08-08 08:51:03 -0300

    primeiro commit

---
+++ app/src/androidTest/java/br/com/alura/orgs/ProdutoActivityTest.kt
@@ -0,0 +1,29 @@
+package br.com.alura.orgs
+
+import androidx.test.core.app.ActivityScenario.launch
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import br.com.alura.orgs.ui.activity.FormularioProdutoActivity
+import br.com.alura.orgs.ui.activity.ListaProdutosActivity
+import org.junit.Test
+
+class ProdutoActivityTest {
+    @Test
+    fun deveMostrarONomeDoAplicativo() {
+        launch(ListaProdutosActivity::class.java)
+        onView(withText("Orgs")).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun deveMostrarCamposNecessarioParaCriarUmProduto() {
+        launch(FormularioProdutoActivity::class.java)
+
+        onView(withId(R.id.activity_formulario_produto_nome)).check(matches(isDisplayed()))
+        onView(withId(R.id.activity_formulario_produto_descricao)).check(matches(isDisplayed()))
+        onView(withId(R.id.activity_formulario_produto_valor)).check(matches(isDisplayed()))
+        onView(withId(R.id.activity_formulario_produto_botao_salvar)).check(matches(isDisplayed()))
+    }
+}
